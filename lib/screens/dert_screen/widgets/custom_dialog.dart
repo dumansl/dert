3f078845d8a8e31d968a01f06668be0098360758
Şule Duman <sduman_816@hotmail.com>
2024-08-26 16:47:53 +0300

    Updated

---
+++ lib/screens/dert_screen/widgets/custom_dialog.dart
@@ -0,0 +1,44 @@
+import 'package:dert/screens/dert_screen/widgets/custom_button.dart';
+import 'package:dert/utils/constant/constants.dart';
+import 'package:flutter/material.dart';
+
+class DialogUtils {
+  static void showMyDialog(
+      BuildContext context, String title, Function() onPressed) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Align(
+            alignment: Alignment.topRight,
+            child: InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                width: 24,
+                height: 24,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(ImagePath.closeLogo),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          content: Text(title,
+              style: DertTextStyle.roboto.t18w700darkpurple,
+              textAlign: TextAlign.center),
+          actions: <Widget>[
+            Align(
+              alignment: Alignment.centerRight,
+              child:
+                  CustomDertButton(text: DertText.send, onPressed: onPressed),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
